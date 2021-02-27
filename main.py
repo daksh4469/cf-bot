ca85488afc456f0a9f100ae2de9eb0d3712e24b5
Rahulag <rahul_a@cs.iitr.ac.in>
2021-02-27 16:47:52 +0530

    add cfbot keyword

--- main.py
+++ main.py
@@ -25,31 +25,35 @@ async def on_ready():
 async def on_message(message):
     msg = message.content
     msgArray = msg.split(" ")
-    if msgArray[0] == "rate":
-        res = requests.get("https://codeforces.com/api/user.info?handles=" + msgArray[1])
-        data = res.json()
-        await message.channel.send(data["result"][0]["maxRating"])
-    if msgArray[0] == "compare":
-        if(len(msgArray)<=2):
-            await message.channel.send("Error: 2 parameters required")
-        else:
-            res1 = requests.get("https://codeforces.com/api/user.info?handles=" + msgArray[1])
-            res2 = requests.get("https://codeforces.com/api/user.info?handles=" + msgArray[2])
-            data1 = res1.json()
-            data2 = res2.json()
-            rating1 = data1["result"][0]["rating"]  
-            rating2 = data2["result"][0]["rating"]
-            r1 = str(rating1)
-            r2 = str(rating2)
-            rank1 = str(data1["result"][0]["rank"])
-            rank2 = str(data2["result"][0]["rank"])
-            if rating2 >= rating1:
-                diff = str(rating2 - rating1)
-                await message.channel.send(msgArray[2] + " has higher rating than " + msgArray[1] + " by a margin of " + diff + "\n" + msgArray[2] + " is a " + rank2 + "(" + r2 + ")" + " and " + msgArray[1] + " is a " + rank1 + "(" + r1 + ")" )
+    if msgArray[0] == "cfbot":
+
+        if msgArray[1] == "rate":
+            res = requests.get("https://codeforces.com/api/user.info?handles=" + msgArray[2])
+            data = res.json()
+            await message.channel.send(data["result"][0]["maxRating"])
+        if msgArray[1] == "compare":
+            if(len(msgArray)<=3):
+                await message.channel.send("Error: 2 parameters required")
             else:
-                diff = str(rating1 - rating2)
-                await message.channel.send(msgArray[1] + " has higher rating than " + msgArray[2] + " by a margin of " + diff + "\n" + msgArray[1] + " is a " + rank1 + "(" + r1 + ")" + " and " + msgArray[2] + " is a " + rank2 + "(" + r2 + ")" )
-    if msgArray[0] == "hello":
-        await message.channel.send("hey dirtbag")
+                res1 = requests.get("https://codeforces.com/api/user.info?handles=" + msgArray[2])
+                res2 = requests.get("https://codeforces.com/api/user.info?handles=" + msgArray[3])
+                data1 = res1.json()
+                data2 = res2.json()
+                rating1 = data1["result"][0]["rating"]  
+                rating2 = data2["result"][0]["rating"]
+                r1 = str(rating1)
+                r2 = str(rating2)
+                rank1 = str(data1["result"][0]["rank"])
+                rank2 = str(data2["result"][0]["rank"])
+                if rating2 >= rating1:
+                    diff = str(rating2 - rating1)
+                    await message.channel.send(msgArray[3] + " has higher rating than " + msgArray[2] + " by a margin of " + diff + "\n" + msgArray[3] + " is a " + rank2 + "(" + r2 + ")" + " and " + msgArray[2] + " is a " + rank1 + "(" + r1 + ")" )
+                else:
+                    diff = str(rating1 - rating2)
+                    await message.channel.send(msgArray[2] + " has higher rating than " + msgArray[3] + " by a margin of " + diff + "\n" + msgArray[2] + " is a " + rank1 + "(" + r1 + ")" + " and " + msgArray[3] + " is a " + rank2 + "(" + r2 + ")" )
+        if msgArray[1] == "hello":
+            await message.channel.send("hey dirtbag")
+    
+   
 
 bot.run(DISCORD_TOKEN)
